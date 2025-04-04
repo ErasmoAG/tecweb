907e93c1b5fc2f4254a43413a658781f8e06c1f5
ErasmoAG <equipo1programacion4a@gmail.com>
2025-04-03 20:38:44 -0600

    Se crearon las carpetas correspondientes con composer

--- practicas/p12/product_app/backend/myapi/DataBase.php
+++ practicas/p12/product_app/backend/myapi/DataBase.php
@@ -3,13 +3,19 @@
 
     abstract class DataBase{
         protected $conexion; 
+        protected $data = NULL; 
 
-        public function  __construct( $user, $pass, $db){
+        public function  __construct($db, $user = 'root', $pass = "GASCAE10M98"){
         $this->conexion = @mysqli_connect('localhost',$user, $pass, $db);
 
             if(!$this->conexion){ 
                 die('¡Base de datos NO conectada!');
             }
+        }
+
+        public function getData(){
+            header('Content-Type: application/json');
+            return json_encode($this->data, JSON_PRETTY_PRINT); 
         }
     }
 ?>
