4a077fb6653b6563ac045f520046e3ec7aca49b6
ErasmoAG <equipo1programacion4a@gmail.com>
2025-04-10 21:24:58 -0600

    Se realizo el ejemplo 3

--- actividades/prueba_slim/index.php
+++ actividades/prueba_slim/index.php
@@ -16,14 +16,21 @@ $app->get('/', function ($request, $response, $args) {
     return $response;
 });
 
-//Ejemplo 2
+
 $app->get("/hola[/{nombre}]", function( $request, $response, $args ){
     $nombre = $args["nombre"] ?? "invitado";
     $response->getBody()->write("Hola, " . $nombre);
     return $response;
 });
 
+$app->post("/pruebapost", function( $request, $response, $args ){
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"] ?? '';
+    $val2 = $reqPost["val2"] ?? '';
 
+    $response->getBody()->write("Valores: " . $val1 . " " . $val2);
+    return $response;
+});
 $app->run();
 
 ?>
